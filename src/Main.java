2addc2e4250dcfbd8cf8d6a754f68001953b62a8
scimmia <affe@live.cn>
2013-09-03 21:52:36 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         // write your code here
+        // write your code here
+        // write your code here
+        // write your code here
 
-    }
+    }                            // write your code here
+
 }
